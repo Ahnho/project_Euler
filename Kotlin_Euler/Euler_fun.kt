ec49ed973515e27f333af154f8a74290eae05419
Ahnho <dksgh00@naver.com>
2019-02-15 16:15:59 +0900

    Initial Commit

--- Kotlin_Euler/Euler_fun.kt
+++
@@ -1,7 +0,0 @@
-import java.math.BigInteger
-import java.util.ArrayList
-import java.util.LinkedList
-
-inline fun Any.toCharList() = toString().toList()
-inline fun Any.toDigits() = toCharList().map { c: Char -> Character.getNumericValue(c) }
-inline fun Any.isPalindrome() = toCharList() == toCharList().reversed()
